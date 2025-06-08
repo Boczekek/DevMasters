719db2d7668f61f877d0ff6f4b41ef104eb84b17
Boczekek <zajack23@interia.pl>
2025-06-08 02:01:50 +0200

    Rozpoczęcie pracy nad GUI

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="shortcut icon" href="images/icon.png">
+        <title>DevMasters</title>
+    </head>
+    <body>
+        <div id="header" class="section">
+            <h1 id="name">&lt;&#47;Dev<b>Masters</b>&gt;</h1>
+            <button id="login" type="button">Zaloguj się</button>
+        </div>
+        <div id="nav" class="section">
+                <button type="button">Home</button>
+                <button type="button">Chat</button>
+                <button type="button">Zamów</button>
+                <button type="button">Kontakt</button>
+                <button type="button">Zlecenia</button>
+                <button type="button">Użytkownicy</button>
+        </div>
+        <div id="main" class="section">
+            <p>Witamy</p>
+        </div>
+        <div id="footer">
+            <p>Krystian Zając</p><p>Wszelkie prawa zastrzeżone</p>
+
+        </div>
+    </body>
+</html>
